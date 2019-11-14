7e042d589673ca3ce083ed6d8044153eb6f9ffb8
Abhishek Mishra <abmishra2111@gmail.com>
2019-11-14 21:20:54 +0530

    Fix: 3 suggestions for mobile

--- src/Common/AutosuggestField.js
+++ src/Common/AutosuggestField.js
@@ -29,8 +29,27 @@ class AutosuggestField extends PureComponent {
     this.state = {
       value: '',
       suggestions: [],
-      suggestionUrl: ''
-    };
+      suggestionUrl: '',
+      isRequestFromMobile: this.isMobilePlatform()
+    };    
+  }
+
+  isMobilePlatform() {
+    if (
+      navigator.userAgent.match(/Android/i) ||
+      navigator.userAgent.match(/webOS/i) ||
+      navigator.userAgent.match(/iPhone/i) ||
+      navigator.userAgent.match(/iPad/i) ||
+      navigator.userAgent.match(/iPod/i) ||
+      navigator.userAgent.match(/BlackBerry/i) ||
+      navigator.userAgent.match(/IEMobile/i) ||
+      navigator.userAgent.match(/WPDesktop/i) ||
+      navigator.userAgent.match(/Windows Phone/i)
+    ) {
+      return true;
+    } else {
+      return false;
+    }
   }
   
   renderInputComponent = (inputProps) => {
