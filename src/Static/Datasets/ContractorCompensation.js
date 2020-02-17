8edc300dfc45783d9e4654264d3ed830062096e4
FeeN Project <feenproject@gmail.com>
2020-02-17 22:06:25 +0300

    Some code improvements

--- src/Static/Datasets/ContractorCompensation.js
+++ src/Static/Datasets/ContractorCompensation.js
@@ -15,51 +15,9 @@ import classNames from 'classnames';
 import { info } from 'App/routes';
 import apiClient from 'App/ApiClient';
 
-const styles = theme => ({
-  root: {
-    ...theme.open990.pageContainer,
-    padding: '0 1.75rem 1.75rem'
-  },
-  policyHeader: {
-    ...theme.open990.pageTitle,
-    margin: '1rem 0',
-    fontSize: 'initial'
-  },
-  container: {
-    display: 'flex',
-    flexDirection: 'column',
-    alignItems: 'center',
-    fontSize: '1.2rem'
-  },
-  textContent: {
-    maxWidth: '80%',
-    fontWeight: 300,
-    lineHeight: 'normal',
-    textAlign: 'center',
-    margin: '0 auto 1.875rem auto'
-  },
-  button: {
-    display: 'block',
-    margin: '1.875rem auto',
-    width: '100%',
-    maxWidth: '30rem',
-    height: '52px',
-    backgroundColor: theme.color.primary.desaturated,
-    '& span': {
-      textTransform: 'Capitalize',
-      color: '#fff',
-      fontSize: '1.125rem',
-      fontWeight: 'bold'
-    },
-    '&:hover': {
-      backgroundColor: theme.color.primary.standard
-    }
-  },
-  accentElement: {
-    color: '#6839d3',
-    cursor: 'pointer'
-  }
-});
+import pageStyles from './pageStyles';
+
+const styles = theme => pageStyles(theme);
 
 const contractorCompensation = ({ classes }) => {
   return (
