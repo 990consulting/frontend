db8997db41ace90e1be4e9861d7a4b11d0cfdf39
David Bruce Borenstein <david@appliednonprofitresearch.com>
2020-02-18 14:23:14 -0500

    Re-adding dataset static pages

---
+++ src/Static/Datasets/ExecutiveCompensation.js
@@ -0,0 +1,167 @@
+/*
+ * Copyright (c) 2019 Open990.org, Inc. All rights reserved.
+ */
+
+import React, { Fragment } from 'react';
+import { Helmet } from 'react-helmet';
+
+import withStyles from '@material-ui/core/styles/withStyles';
+import Button from '@material-ui/core/Button';
+import Grid from '@material-ui/core/Grid';
+
+import MaxContainer from 'hoc/MaxContainer';
+import classNames from 'classnames';
+
+import { info } from 'App/routes';
+
+const styles = theme => ({
+  root: {
+    ...theme.open990.pageContainer,
+    padding: '0 1.75rem 1.75rem'
+  },
+  policyHeader: {
+    ...theme.open990.pageTitle,
+    margin: '1rem 0',
+    fontSize: 'initial',
+    '& h2': {
+      fontWeight: '300',
+      fontStyle: 'italic'
+    }
+  },
+  container: {
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+    fontSize: '1.2rem'
+  },
+  textContent: {
+    maxWidth: '80%',
+    fontWeight: 300,
+    lineHeight: 'normal',
+    textAlign: 'center',
+    margin: '0 auto 1.875rem auto'
+  },
+  contentList: {
+    margin: '0 auto 1.875rem auto',
+    fontSize: '1.06rem',
+    maxWidth: '40rem',
+    listStyleType: 'none',
+    '& li': {
+      margin: '0 0 0.5rem',
+      textAlign: 'left',
+      '&:before': {
+        content: `'\\2014'`,
+        position: 'absolute',
+        marginLeft: '-20px'
+      }
+    }
+  },
+  button: {
+    display: 'block',
+    margin: '0 auto 1.875rem',
+    width: '100%',
+    maxWidth: '30rem',
+    height: '52px',
+    backgroundColor: theme.color.primary.desaturated,
+    '& span': {
+      textTransform: 'Capitalize',
+      color: '#fff',
+      fontSize: '1.125rem',
+      fontWeight: 'bold'
+    },
+    '&:hover': {
+      backgroundColor: theme.color.primary.standard
+    }
+  },
+  accentElement: {
+    color: '#6839d3',
+    cursor: 'pointer'
+  }
+});
+
+const executiveCompensation = ({ classes }) => {
+  return (
+    <Fragment>
+      <Helmet>
+        <title>2020 Compensation Dataset for Nonprofits | Open990</title>
+        <meta
+          name="description"
+          content="Open990.org has the most in-depth nonprofit compensation dataset based on IRS data. And it’s free!"
+        />
+        <meta name="robots" content="all" />
+        <link rel="canonical" href="https://www.open990.org/contact/" />
+      </Helmet>
+      <div className={classNames('DatasetPage', classes.root)}>
+        <MaxContainer classes={{ container: classes.container }}>
+          <Grid container justify="center">
+            <Grid item xs={12} md={10} className={classes.policyHeader}>
+              <h1>The 2020 Open990 Compensation Dataset for Nonprofits</h1>
+              <h2>Your source for data-driven compensation decision-making</h2>
+            </Grid>
+            <Grid item xs={12}>
+              <Button
+                  className={classes.button}
+                  onClick={() => null}
+                  id="landingpage-subscribe-executive"
+              >
+                Get notified when it’s ready
+              </Button>
+              <p className={classes.textContent}>
+                Open990.org has the most in-depth nonprofit compensation dataset
+                based on IRS data. And it’s free!*
+              </p>
+            </Grid>
+            <Grid container justify="center">
+              <ul className={classes.contentList}>
+                <li>
+                  Includes job titles, hours, position, and 19 compensation
+                  metrics
+                </li>
+                <li>
+                  Covers officers, directors, trustees, key employees and
+                  highest compensated employees
+                </li>
+                <li>
+                  Data from electronically-filed Form 990, 990-EZ, 990-PF, and
+                  Schedule J filings for TY 2018
+                </li>
+                <li>
+                  Individual-level data in spreadsheet format make it easy for
+                  you to search, sort, and answer the questions important to
+                  your organization
+                </li>
+                <li>
+                  Analyze by gender, geography, cause area, assets, and more
+                </li>
+              </ul>
+            </Grid>
+            <Grid item xs={12} className={classes.headingContent}>
+              <small style={{ display: 'block', marginBottom: '1.875rem' }}>
+                *Free for non-commercial use only, with attribution to
+                Open990.org. Contact{' '}
+                <a href={info} className={classes.accentElement}>
+                  info@open990.org
+                </a>{' '}
+                if you have questions about your use case.
+              </small>
+              <p className={classes.textContent}>
+                Nonprofits: create your own nonprofit executive compensation
+                report, benchmarking compensation against similar organizations
+                <br />
+                Researchers: uncover patterns in nonprofit compensation
+                <br />
+                Consultants: these data are not free for commercial use
+              </p>
+              <small style={{ fontStyle: 'italic' }}>
+                Open990.org is a 501(c)3 organization committed to facilitating
+                research and promoting transparency in the nonprofit sector.
+              </small>
+            </Grid>
+          </Grid>
+        </MaxContainer>
+      </div>
+    </Fragment>
+  );
+};
+
+export default withStyles(styles)(executiveCompensation);
---
+++ src/Static/Datasets/FoundationsAndGrants.js
@@ -0,0 +1,170 @@
+/*
+ * Copyright (c) 2019 Open990.org, Inc. All rights reserved.
+ */
+
+import React, { Fragment } from 'react';
+import { Helmet } from 'react-helmet';
+
+import withStyles from '@material-ui/core/styles/withStyles';
+import Button from '@material-ui/core/Button';
+import Grid from '@material-ui/core/Grid';
+
+import MaxContainer from 'hoc/MaxContainer';
+import classNames from 'classnames';
+
+import { info } from 'App/routes';
+import apiClient from 'App/ApiClient';
+
+const styles = theme => ({
+  root: {
+    ...theme.open990.pageContainer,
+    padding: '0 1.75rem 1.75rem'
+  },
+  policyHeader: {
+    ...theme.open990.pageTitle,
+    margin: '1rem 0',
+    fontSize: 'initial'
+  },
+  container: {
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+    fontSize: '1.2rem'
+  },
+  textContent: {
+    maxWidth: '80%',
+    fontWeight: 300,
+    lineHeight: 'normal',
+    textAlign: 'center',
+    margin: '0 auto 1.875rem auto'
+  },
+  contentList: {
+    margin: '0 auto 1.875rem auto',
+    fontSize: '1.06rem',
+    '& li': {
+      margin: '0 0 0.5rem',
+      textAlign: 'left'
+    }
+  },
+  button: {
+    display: 'block',
+    margin: '1.875rem auto',
+    width: '100%',
+    maxWidth: '30rem',
+    height: '52px',
+    backgroundColor: theme.color.primary.desaturated,
+    '& span': {
+      textTransform: 'Capitalize',
+      color: '#fff',
+      fontSize: '1.125rem',
+      fontWeight: 'bold'
+    },
+    '&:hover': {
+      backgroundColor: theme.color.primary.standard
+    }
+  },
+  accentElement: {
+    color: '#6839d3',
+    cursor: 'pointer'
+  }
+});
+
+const foundationsAndGrants = ({ classes }) => {
+  return (
+    <Fragment>
+      <Helmet>
+        <title>Foundations And Grants</title>
+        <meta
+          name="description"
+          content="Streamline your grant search with Open990's free Foundations & Grants dataset. Contains historical grants, contact info, and application guidelines."
+        />
+        <meta name="robots" content="all" />
+        <link rel="canonical" href="https://www.open990.org/dataset/foundations_and_grants/" />
+      </Helmet>
+      <div className={classNames('DatasetPage', classes.root)}>
+        <MaxContainer classes={{ container: classes.container }}>
+          <Grid container justify="center">
+            <Grid item xs={12} md={10} className={classes.policyHeader}>
+              <h1>The dataset you need to get the grants you want.</h1>
+            </Grid>
+            <Grid item xs={12}>
+              <p className={classes.textContent}>
+                Streamline your nonprofit grant search with Open990’s free*
+                Foundations & Grants dataset.
+              </p>
+            </Grid>
+            <Grid container justify="center">
+              <ol className={classes.contentList}>
+                <li>
+                  identify foundations whose charitable activities are aligned
+                  with your organization’s mission
+                </li>
+                <li>
+                  browse grants recently awarded by those foundations
+                  (recipients, descriptions, amounts)
+                </li>
+                <li>
+                  find application submission information (deadlines, contact
+                  info)
+                </li>
+              </ol>
+            </Grid>
+            <Grid item xs={12} className={classes.headingContent}>
+              <p className={classes.textContent}>
+                <strong>
+                  Sort, filter, and analyze funding prospects by geography,
+                  grant maximums, grant counts, total disbursements, cause area,
+                  and more!
+                </strong>
+              </p>
+              <p className={classes.textContent} style={{ color: '#6839d3' }}>
+                Designed with fundraisers in mind, this dataset comprises
+                123,000 U.S. private foundations, including contact information
+                and assets. It features 913,00 grants awarded by foundations
+                that electronically filed a tax return for TY 2017 or 2018.
+              </p>
+              <small>
+                *Free for non-commercial use only, such as grant-seeking,
+                research, and journalism. Contact{' '}
+                <a href={info} className={classes.accentElement}>
+                  info@open990.org
+                </a>{' '}
+                if you have questions about your use case.
+              </small>
+              <Button
+                id="landingpage-download-foundation"
+                className={classes.button}
+                onClick={() =>
+                  apiClient.doDownload(
+                    'Open990_SnackSet_Foundations_Grants.zip'
+                  )
+                }
+              >
+                Download now!
+              </Button>
+              <p className={classes.textContent}>
+                <span
+                  className={classes.accentElement}
+                  onClick={() =>
+                    apiClient.doDownload(
+                      'Open990_SnackSet_Foundations_Grants.zip'
+                    )
+                  }
+                >
+                  Download
+                </span>{' '}
+                contains the dataset (2 csv files), license, and documentation.
+              </p>
+              <small style={{ fontStyle: 'italic' }}>
+                Open990.org is a 501(c)3 organization committed to facilitating
+                research and promoting transparency in the nonprofit sector.
+              </small>
+            </Grid>
+          </Grid>
+        </MaxContainer>
+      </div>
+    </Fragment>
+  );
+};
+
+export default withStyles(styles)(foundationsAndGrants);
