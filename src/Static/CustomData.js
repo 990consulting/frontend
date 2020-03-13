37563e6385ca76d119aa3e7614528d2bc5e4b6f2
FeeN Project <feenproject@gmail.com>
2020-03-13 22:14:28 +0300

    Small CSS Fixes

--- src/Static/CustomData.js
+++ src/Static/CustomData.js
@@ -18,23 +18,38 @@ import MaxContainer from 'hoc/MaxContainer';
 import ContactForm from 'Common/ContactForm';
 import IconCard from 'Common/IconCard';
 
-const styles = (theme) => ({
+const styles = theme => ({
   root: {
     ...theme.open990.pageContainer
   },
   underlinedHeader: {
     margin: '0 0 1.875rem 0',
     padding: 0,
-    '& h3': {
+    '& div': {
+      display: 'flex',
       padding: '0 0 0.625rem',
-      margin: 0,
+      justifyContent: 'center',
+      alignItems: 'center',
       borderBottom: `2px solid ${theme.color.primary.desaturated}`,
-      fontSize: '1.5rem',
+      '& h3, ul': {
+        margin: 0,
+        fontSize: '1.5rem'
+      },
       letterSpacing: '0.033rem',
-      '& span':{
-        margin: '0 1rem'
+      '& ul': {
+        display: 'flex',
+        fontFamily: `'Lato', sans-serif`,
+        fontWeight: 'bold',
+        '& li': {
+          marginRight: '2rem'
+        }
+      },
+      [theme.breakpoints.down(1005)]: {
+        '&, ul': {
+          flexDirection: 'column'
+        }
       }
-    },
+    }
   },
   featuresElement: {
     border: `1px solid ${theme.color.grey.standard}`,
@@ -50,7 +65,7 @@ const styles = (theme) => ({
       border: `1px solid ${theme.color.grey.standard}`,
       borderRadius: '50%',
       backgroundColor: theme.color.grey.desaturated,
-      color: theme.color.black,
+      color: theme.color.black
     }
   },
   iconCardRibbon: {
@@ -61,6 +76,9 @@ const styles = (theme) => ({
   },
   contactForm: {
     padding: '1rem 2rem',
+    '& #formSubmit': {
+      maxHeight: '2.25rem'
+    },
     [theme.breakpoints.only('sm')]: {
       margin: '0 5rem'
     }
@@ -68,7 +86,8 @@ const styles = (theme) => ({
   dottedHeader: {
     '& h2': {
       backgroundColor: theme.color.desaturated,
-      background: 'url(https://open990-assets.s3.amazonaws.com/css/images/icons/dotted.png) repeat-x center',
+      background:
+        'url(https://open990-assets.s3.amazonaws.com/css/images/icons/dotted.png) repeat-x center',
       paddingRight: '1rem',
       paddingLeft: '1rem',
       marginTop: '1.5rem',
@@ -76,7 +95,7 @@ const styles = (theme) => ({
       fontSize: '24px',
       fontWeight: 600,
       lineHeight: 1.5,
-      margin: '0 0 30px 0',
+      margin: '0 0 30px 0'
     }
   },
   lineHeader: {
@@ -90,7 +109,7 @@ const styles = (theme) => ({
   listNumber: {
     fontSize: '3rem',
     color: theme.color.primary.desaturated,
-    textAlign: 'center',
+    textAlign: 'center'
   },
   listTitle: {
     fontSize: '1rem',
@@ -111,7 +130,7 @@ const styles = (theme) => ({
     textAlign: 'left',
     [theme.breakpoints.down('sm')]: {
       padding: '1rem 0'
-    },
+    }
   },
   paper: {
     lineHeight: 1.5,
@@ -133,21 +152,46 @@ const CustomData = ({
     <Fragment>
       <Helmet>
         <title>Nonprofit Custom Datasets | Open990.org</title>
-        <meta name="description" content="For researchers, grantseekers, marketers, and nonprofit leaders. Choose from hundreds of variables." />
-        <meta name="robots" content="all"/>
-        <link rel="canonical" href="https://appliednonprofitresearch.com/customdata/" />
+        <meta
+          name="description"
+          content="For researchers, grantseekers, marketers, and nonprofit leaders. Choose from hundreds of variables."
+        />
+        <meta name="robots" content="all" />
+        <link
+          rel="canonical"
+          href="https://appliednonprofitresearch.com/customdata/"
+        />
       </Helmet>
       <div className={classes.root}>
         <MaxContainer>
-          <Grid container className={classes.staticMainContent} justify="center">
+          <Grid
+            container
+            className={classes.staticMainContent}
+            justify="center"
+          >
             <Grid item xs={12} md={10} className={classes.underlinedHeader}>
-              <h3>IRS nonprofit data <span>•</span> 1.9 million organizations <span>•</span> 5.4 million filings</h3>
+              <div>
+                <h3>IRS nonprofit data</h3>
+                <ul>
+                  <li>1.9 million organizations</li>
+                  <li>5.4 million filings</li>
+                </ul>
+              </div>
             </Grid>
             <Grid item xs={12} md={10} className={classes.lineText}>
-              <p>Researchers and nonprofit professionals may wish to purchase custom datasets that include variables
-                displayed on Open990 or hundreds of other variables from the suite of IRS Forms 990.</p>
+              <p>
+                Researchers and nonprofit professionals may wish to purchase
+                custom datasets that include variables displayed on Open990 or
+                hundreds of other variables from the suite of IRS Forms 990.
+              </p>
             </Grid>
-            <Grid item xs={12} sm={8} md={10} className={classes.iconCardRibbon}>
+            <Grid
+              item
+              xs={12}
+              sm={8}
+              md={10}
+              className={classes.iconCardRibbon}
+            >
               <Grid container spacing={24}>
                 <Grid item xs={12} md={4}>
                   <IconCard
@@ -155,7 +199,9 @@ const CustomData = ({
                       extendedPaper: classes.paper
                     }}
                     icon={faFilter}
-                    text={'Apply simple data filters so that you pay only for organizations that meet your criteria'}
+                    text={
+                      'Apply simple data filters so that you pay only for organizations that meet your criteria'
+                    }
                   />
                 </Grid>
                 <Grid item xs={12} md={4}>
@@ -164,7 +210,9 @@ const CustomData = ({
                       extendedPaper: classes.paper
                     }}
                     icon={faCogs}
-                    text={'Supply data from Forms 990 (core), EZ, PF, and most schedules, e-filed in 2011 – present'}
+                    text={
+                      'Supply data from Forms 990 (core), EZ, PF, and most schedules, e-filed in 2011 – present'
+                    }
                   />
                 </Grid>
                 <Grid item xs={12} md={4}>
@@ -173,23 +221,37 @@ const CustomData = ({
                       extendedPaper: classes.paper
                     }}
                     icon={faDownload}
-                    text={'Provide the custom extract in delimited (.csv) or Excel (.xlsx) format'}
+                    text={
+                      'Provide the custom extract in delimited (.csv) or Excel (.xlsx) format'
+                    }
                   />
                 </Grid>
               </Grid>
             </Grid>
             <Grid item xs={12} md={10} className={classes.lineText}>
-              <p>The most data are available for organizations that file tax returns electronically, but some data (e.g.,
-                total assets, NTEE codes) are available for organizations that paper-file 990s.</p>
+              <p>
+                The most data are available for organizations that file tax
+                returns electronically, but some data (e.g., total assets, NTEE
+                codes) are available for organizations that paper-file 990s.
+              </p>
             </Grid>
-            <Grid item xs={12} md={10} className={classNames(classes.lineHeader, classes.newLineHeader)}>
+            <Grid
+              item
+              xs={12}
+              md={10}
+              className={classNames(classes.lineHeader, classes.newLineHeader)}
+            >
               <h1>Identify trends</h1>
             </Grid>
             <Grid item xs={12} md={10} className={classes.lineText}>
-              <p>We track over 350 dimensions on tax-exempt organizations from 2010 to present. Our proprietary records
-                linkage algorithm identifies amounts spent on individual people and programs over time – even if they are
-                written differently on each tax return. This reveals trends in executive compensation, program expenses,
-                program revenues, and more.</p>
+              <p>
+                We track over 350 dimensions on tax-exempt organizations from
+                2010 to present. Our proprietary records linkage algorithm
+                identifies amounts spent on individual people and programs over
+                time – even if they are written differently on each tax return.
+                This reveals trends in executive compensation, program expenses,
+                program revenues, and more.
+              </p>
             </Grid>
             <Grid item sm={10}>
               <Grid container>
@@ -198,54 +260,101 @@ const CustomData = ({
                 </Grid>
                 <Grid item xs={12} className={classes.listBlock}>
                   <Grid container>
-                    <Grid item xs={12} md={4} className={classes.numberedListItem}>
+                    <Grid
+                      item
+                      xs={12}
+                      md={4}
+                      className={classes.numberedListItem}
+                    >
                       <Grid container>
                         <Grid item xs={2} className={classes.listNumber}>
                           1.
                         </Grid>
                         <Grid item xs={10}>
                           <Grid container>
-                            <Grid item xs={12} md={10} className={classes.listTitle}>
+                            <Grid
+                              item
+                              xs={12}
+                              md={10}
+                              className={classes.listTitle}
+                            >
                               Simplify structure
                             </Grid>
-                            <Grid item xs={12} md={10} className={classes.listText}>
-                              Our data scientists compare tax forms to bring together related variables.
+                            <Grid
+                              item
+                              xs={12}
+                              md={10}
+                              className={classes.listText}
+                            >
+                              Our data scientists compare tax forms to bring
+                              together related variables.
                             </Grid>
                           </Grid>
                         </Grid>
                       </Grid>
                     </Grid>
-                    <Grid item xs={12} md={4} className={classes.numberedListItem}>
+                    <Grid
+                      item
+                      xs={12}
+                      md={4}
+                      className={classes.numberedListItem}
+                    >
                       <Grid container>
                         <Grid item xs={2} className={classes.listNumber}>
                           2.
                         </Grid>
                         <Grid item xs={10}>
                           <Grid container>
-                            <Grid item xs={12} md={10} className={classes.listTitle}>
+                            <Grid
+                              item
+                              xs={12}
+                              md={10}
+                              className={classes.listTitle}
+                            >
                               Match across years
                             </Grid>
-                            <Grid item xs={12} md={10} className={classes.listText}>
-                              Our technology links records on individuals and programs, even if they are written
-                              slightly
+                            <Grid
+                              item
+                              xs={12}
+                              md={10}
+                              className={classes.listText}
+                            >
+                              Our technology links records on individuals and
+                              programs, even if they are written slightly
                               differently each year.
                             </Grid>
                           </Grid>
                         </Grid>
                       </Grid>
                     </Grid>
-                    <Grid item xs={12} md={4} className={classes.numberedListItem}>
+                    <Grid
+                      item
+                      xs={12}
+                      md={4}
+                      className={classes.numberedListItem}
+                    >
                       <Grid container>
                         <Grid item xs={2} className={classes.listNumber}>
                           3.
                         </Grid>
                         <Grid item xs={10}>
                           <Grid container>
-                            <Grid item xs={12} md={10} className={classes.listTitle}>
+                            <Grid
+                              item
+                              xs={12}
+                              md={10}
+                              className={classes.listTitle}
+                            >
                               Derive new variables
                             </Grid>
-                            <Grid item xs={12} md={10} className={classes.listText}>
-                              We compute new variables to add to the dataset, e.g., fundraising efficiency.
+                            <Grid
+                              item
+                              xs={12}
+                              md={10}
+                              className={classes.listText}
+                            >
+                              We compute new variables to add to the dataset,
+                              e.g., fundraising efficiency.
                             </Grid>
                           </Grid>
                         </Grid>
@@ -256,24 +365,30 @@ const CustomData = ({
               </Grid>
             </Grid>
             <Grid item xs={12} md={10} className={classes.lineText}>
-              <p>Data sources include IRS Forms 990, 990-PF, 990-EZ, schedules, IRS Business Master File, and National
-                Center for Charitable Statistics. </p>
+              <p>
+                Data sources include IRS Forms 990, 990-PF, 990-EZ, schedules,
+                IRS Business Master File, and National Center for Charitable
+                Statistics.{' '}
+              </p>
             </Grid>
             <Grid item xs={12} md={10}>
               <ContactForm
                 classes={{
                   extendedForm: classes.contactForm
                 }}
-                headerText={'Interested in using nonprofit financial data in your work?'}
+                headerText={
+                  'Interested in using nonprofit financial data in your work?'
+                }
                 subHeaderText={'Contact us to discuss your goals.'}
                 formName="custom datasets"
-                direction={'row'}/>
+                direction={'row'}
+              />
             </Grid>
           </Grid>
         </MaxContainer>
       </div>
     </Fragment>
-  )
+  );
 };
 
 export default withStyles(styles)(CustomData);
